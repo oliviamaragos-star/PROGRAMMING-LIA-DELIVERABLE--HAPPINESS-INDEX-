e30039830c0a4c631f4fcdbb37fa3322453094e8
oliviamaragos-star <olivia.maragos@gmail.com>
2025-10-05 12:50:23 -0400

    Create LIA CODE-HAPPINESS INDEX.py

---
+++ LIA CODE-HAPPINESS INDEX.py
@@ -0,0 +1,9 @@
+# ================================================================
+# File: LIA CODE-HAPPINESS INDEX.py
+# Author: OLIVIA MARAGOS and VICTORIA MILIOTO 
+# Course: Python Programming / Lab 05
+# Date: October 2025
+#
+# Description:
+#  This program contains all exercises for the Lia Deliverable .
+#  It will be used to analyze the dataset of the WHI using GitHub.
