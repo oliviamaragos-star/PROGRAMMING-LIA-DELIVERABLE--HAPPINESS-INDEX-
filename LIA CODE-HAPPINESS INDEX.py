ed89763631218f7247748ade5a40e6c4d130b1ef
oliviamaragos-star <olivia.maragos@gmail.com>
2025-10-05 12:52:35 -0400

    Update LIA CODE-HAPPINESS INDEX.py

--- LIA CODE-HAPPINESS INDEX.py
+++ LIA CODE-HAPPINESS INDEX.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # ================================================================
 # File: LIA CODE-HAPPINESS INDEX.py
 # Author: OLIVIA MARAGOS and VICTORIA MILIOTO 
@@ -8,14 +7,3 @@
 # Description:
 #  This program contains all exercises for the Lia Deliverable .
 #  It will be used to analyze the dataset of the WHI using GitHub.
-=======
-# ================================================================
-# File: LIA CODE-HAPPINESS INDEX.py
-# Author: OLIVIA MARAGOS and VICTORIA MILIOTO 
-# Course: Python Programming / Lab 05
-# Date: October 2025
-#
-# Description:
-#  This program contains all exercises for the Lia Deliverable .
-#  It will be used to analyze the dataset of the WHI using GitHub.
->>>>>>> 72dd71a72edbc3a5b90feb398dc2a3bf507e2e44
